bc5d74b809c2f987750110c19e49c4e3f18c5eb6
Dhanush <dhanush@alphadevs.com>
2017-05-28 14:58:36 +0530

    Initial Commit - Basic Structure

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,52 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { PlayAroundSharedModule, UserRouteAccessService } from './shared';
+import { PlayAroundHomeModule } from './home/home.module';
+import { PlayAroundAdminModule } from './admin/admin.module';
+import { PlayAroundAccountModule } from './account/account.module';
+import { PlayAroundEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        PlayAroundSharedModule,
+        PlayAroundHomeModule,
+        PlayAroundAdminModule,
+        PlayAroundAccountModule,
+        PlayAroundEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class PlayAroundAppModule {}
